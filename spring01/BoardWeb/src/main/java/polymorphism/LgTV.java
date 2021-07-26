8a1f4d7dcf7624470798064fde705668555bfe93
DESKTOP-63NPBHK\JK <kimjin.dev@gmail.com>
2021-07-26 22:58:02 +0900

    Annotation;@Resource

--- spring01/BoardWeb/src/main/java/polymorphism/LgTV.java
+++ spring01/BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -1,13 +1,14 @@
 package polymorphism;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("tv")
 public class LgTV implements TV{
-	@Autowired
-	@Qualifier("apple")
+	@Resource(name="sony")
 	private Speaker speacker;
 	
 	public LgTV() {
