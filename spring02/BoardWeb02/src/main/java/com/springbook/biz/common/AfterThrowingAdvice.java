6ac10cba0c06adb8c22dc6f172b88bc8c89c5535
DESKTOP-63NPBHK\JK <kimjin.dev@gmail.com>
2021-08-05 17:00:27 +0900

    After Throwing 어드바이스

--- spring02/BoardWeb02/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
+++ spring02/BoardWeb02/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
@@ -1,13 +1,24 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
+@Aspect
 public class AfterThrowingAdvice {
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {
+	}
+	
+	@AfterThrowing(pointcut="allPointcut()", throwing="exceptObj")
 	public void exceptionLog(JoinPoint jp, Exception exceptObj) {
 		String method = jp.getSignature().getName();
-
-//		System.out.println("[예외 처리]" + method 
-//				+ "() 메소드 실행 중 발생된 예외 메시지 : " + exceptObj.getMessage());
+		
+		// System.out.println("[예외 처리] " + method + "() 메소드 수행 중 발생된 예외 메시지 : "
+		// + exceptObj.getMessage());
 		System.out.println(method + "() 메소드 수행 중 예외 발생!");
 		if (exceptObj instanceof IllegalArgumentException) {
 			System.out.println("부적합한 값이 입력되었습니다.");
@@ -17,5 +28,4 @@ public class AfterThrowingAdvice {
 			System.out.println("문제가 발생했습니다.");
 		}
 	}
-
 }
