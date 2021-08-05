85f86621b6c3510770001611f71fdd8dc22e6088
DESKTOP-63NPBHK\JK <kimjin.dev@gmail.com>
2021-08-05 14:24:02 +0900

    횡단관심과 핵심관심1-1

--- spring02/BoardWeb02/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ spring02/BoardWeb02/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -8,39 +8,47 @@ import org.springframework.stereotype.Service;
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.common.LogAdvice;
+import com.springbook.biz.common.Log4jAdvice;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 	@Autowired
 	private BoardDAO boardDAO;
-	private LogAdvice log;
+//	private LogAdvice log;
+	private Log4jAdvice log;
 
 	public BoardServiceImpl() {
-		log = new LogAdvice();
+//		log = new LogAdvice();
+		log = new Log4jAdvice();
 	}
 
 	public void insertBoard(BoardVO vo) {
-		log.printLog();
+//		log.printLog();
+		log.printLogging();
 		boardDAO.insertBoard(vo);
 	}
 
 	public void updateBoard(BoardVO vo) {
-		log.printLog();
+//		log.printLog();
+		log.printLogging();
 		boardDAO.updateBoard(vo);
 	}
 
 	public void deleteBoard(BoardVO vo) {
-		log.printLog();
+//		log.printLog();
+		log.printLogging();
 		boardDAO.deleteBoard(vo);
 	}
 
 	public BoardVO getBoard(BoardVO vo) {
-		log.printLog();
+//		log.printLog();
+		log.printLogging();
 		return boardDAO.getBoard(vo);
 	}
 
 	public List<BoardVO> getBoardList(BoardVO vo) {
-		log.printLog();
+//		log.printLog();
+		log.printLogging();
 		return boardDAO.getBoardList(vo);
 	}
 }
