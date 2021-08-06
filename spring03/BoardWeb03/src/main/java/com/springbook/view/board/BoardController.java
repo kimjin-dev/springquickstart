63cc699954f724183726d16946a9a233b84c00d1
DESKTOP-63NPBHK\JK <kimjin.dev@gmail.com>
2021-08-06 23:36:10 +0900

    @ModelAttribute의 수행 순서와 model 제어

--- spring03/BoardWeb03/src/main/java/com/springbook/view/board/BoardController.java
+++ spring03/BoardWeb03/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,15 +1,28 @@
 package com.springbook.view.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
 public class BoardController {
+	
+	// 검색 조건 목록 설정
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap(){
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		return conditionMap;
+	}
+	
 	// 글 등록
 	@RequestMapping(value = "/insertBoard.do")
 	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
@@ -40,14 +53,9 @@ public class BoardController {
 
 	// 글 목록 검색
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(
-			@RequestParam(value = "searchCondition", defaultValue = "TITLE", required = false) String condition,
-			@RequestParam(value = "searchKeyword", defaultValue = "", required = false) String keyword,
-			BoardDAO boardDAO, Model model) {
-		System.out.println("검색 조건 : " + condition);
-		System.out.println("검색 단어 : " + keyword);
+	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {		
 		// Model 정보 저장
-//		model.addAttribute("boardList", boardDAO.getBoardList(vo));																
+		model.addAttribute("boardList", boardDAO.getBoardList(vo));																
 		return "getBoardList.jsp"; // View 이름 리턴
 	}
 }
