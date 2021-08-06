a1262774cdab840c639386fbf34bdc02e9ab81c8
DESKTOP-63NPBHK\JK <kimjin.dev@gmail.com>
2021-08-06 14:27:47 +0900

    Annotation based board

--- spring03/BoardWeb03/src/main/java/com/springbook/view/board/InsertBoardController.java
+++ spring03/BoardWeb03/src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -1,8 +1,5 @@
 package com.springbook.view.board;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,16 +7,11 @@ import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
-public class InsertBoardController {
+public class InsertBoardController{
 	
 	@RequestMapping(value="/insertBoard.do")
-	public void insertBoard(BoardVO vo) {
-		System.out.println("글 등록 처리");
-		
-		// 1. 사용자 입력 정보 추출
-		// 2. DB 연동 처리
-		BoardDAO boardDAO = new BoardDAO();
-		boardDAO.insertBoard(vo);
-		
+	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {		
+		boardDAO.insertBoard(vo);	
+		return "redirect:getBoardList.do";
 	}
 }
