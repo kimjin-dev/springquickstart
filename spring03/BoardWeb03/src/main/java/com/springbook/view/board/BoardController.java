0ef484eac2df19bd44f50109796ab8fdbeb84f42
DESKTOP-63NPBHK\JK <kimjin.dev@gmail.com>
2021-08-06 23:30:35 +0900

    command 객체에 없는 파라미터 사용

--- spring03/BoardWeb03/src/main/java/com/springbook/view/board/BoardController.java
+++ spring03/BoardWeb03/src/main/java/com/springbook/view/board/BoardController.java
@@ -3,6 +3,7 @@ package com.springbook.view.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
@@ -33,14 +34,20 @@ public class BoardController {
 	// 글 상세 조회
 	@RequestMapping("/getBoard.do")
 	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
-		model.addAttribute("board", boardDAO.getBoard(vo));	// Model 정보 저장 
-		return "getBoard.jsp"; 	//View 이름 리턴
+		model.addAttribute("board", boardDAO.getBoard(vo)); // Model 정보 저장
+		return "getBoard.jsp"; // View 이름 리턴
 	}
 
 	// 글 목록 검색
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
-		model.addAttribute("boardList", boardDAO.getBoardList(vo)); // Model 정보 저장
-		return "getBoardList.jsp";  //View 이름 리턴
+	public String getBoardList(
+			@RequestParam(value = "searchCondition", defaultValue = "TITLE", required = false) String condition,
+			@RequestParam(value = "searchKeyword", defaultValue = "", required = false) String keyword,
+			BoardDAO boardDAO, Model model) {
+		System.out.println("검색 조건 : " + condition);
+		System.out.println("검색 단어 : " + keyword);
+		// Model 정보 저장
+//		model.addAttribute("boardList", boardDAO.getBoardList(vo));																
+		return "getBoardList.jsp"; // View 이름 리턴
 	}
 }
